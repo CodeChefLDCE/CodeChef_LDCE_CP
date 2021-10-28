a9b6a8738032bf43d0503598aae1910c41080d7c
Qaisarali Sulaimani <72247744+Qaisarali-Sulaimani@users.noreply.github.com>
2021-10-28 16:35:37 +0530

    Added Binary exponention

---
+++ Graphs/binary_exponention.cpp
@@ -0,0 +1,25 @@
+long long bpow(long long a, long long b, long long mod)
+{
+    long long ans = 1;
+
+    while(b > 0)
+    {
+        if(b % 2 == 0)
+        {
+            a = ((a % mod) * 1LL * (a % mod)) % mod;
+            b = (b) / 2;
+
+        }
+
+        else
+        {
+            ans = ((ans % mod) * 1LL * (a % mod)) % mod;
+            b--;
+
+        }
+
+    }
+
+    return ans % mod;
+
+}
