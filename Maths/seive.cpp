11da3982a49134ed11a92457edcdb79f03335bd2
Qaisarali Sulaimani <72247744+Qaisarali-Sulaimani@users.noreply.github.com>
2021-10-28 16:47:35 +0530

    Update seive.cpp

--- Maths/seive.cpp
+++ Maths/seive.cpp
@@ -1,23 +1,21 @@
 #define LIM x
 
-bool is_prime[LIM+1];
- 
+bool is_prime[LIM + 1];
+
 void seive()
 {
     is_prime[0] = is_prime[1] = 1;
- 
-    for (ll i = 2; (ll)(i*i)<=(LIM); i++)
-    {
-        if (is_prime[i]==0)
-        {
-            for (ll j = i * i; j <= LIM; j += i)
-            {
-                is_prime[j]=1;
 
-            }
+    for (long long i = 2; (long long)(i * i) <= (LIM); i++)
+    {
+        if (is_prime[i] == 0)
+        {
+            for (long long j = i * i; j <= LIM; j += i)
+                is_prime[j] = 1;
+
 
         }
- 
+
     }
- 
+
 }
